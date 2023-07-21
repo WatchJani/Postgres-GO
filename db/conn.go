5935008d4921c68b8d81ffab4c9ca4e68c619d62
Janko <45310863+WatchJani@users.noreply.github.com>
2023-07-21 15:35:38 +0200

    Update conn.go

--- db/conn.go
+++ db/conn.go
@@ -9,7 +9,7 @@ var Conn *sql.DB
 
 func Connect() {
 	var err error
-	Conn, err = sql.Open("postgres", "user=janko dbname=company password=JankoKondic72621@ sslmode=disable")
+	Conn, err = sql.Open("postgres", "")
 	if err != nil {
 		log.Fatal(err)
 	}
